28e0392f749d85aed6966f7272c03507d3415385
Sean Pietersen <seanpietersen7@gmail.com>
2022-04-21 09:38:46 +0200

    Feat: Added Test

---
+++ NthFibonacci/NthFibonacci.Test/CalculateTheNthFibonacciNumberTest.cs
@@ -0,0 +1,26 @@
+using Xunit;
+
+namespace NthFibonacci.Test
+{
+    public class CalculateTheNthFibonacciNumberTest
+    {
+        [Theory]
+        [InlineData(1, 0)]
+        [InlineData(2, 1)]
+        [InlineData(3, 1)]
+        [InlineData(4, 2)]
+
+        public void NthFibTest1(int n, int expected)
+        {
+            //Arrange
+            ICalculateTheNthFibonacciNumber calculateTheNthFibonacci = new CalculateTheNthFibonacciNumber();
+
+            //Act
+            var actual = calculateTheNthFibonacci.NthFib(n);
+
+            //Assert
+            Assert.Equal(expected, actual);
+
+        }
+    }
+}
